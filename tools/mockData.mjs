dbe89559c3e21e83cc6f0e3ac3a3ecd10f41d170
John Bogart <j.bogart91@gmail.com>
2023-07-26 19:58:56 +0100

    Add some more mock data

--- tools/mockData.mjs
+++ tools/mockData.mjs
@@ -4,6 +4,20 @@ const movies = [
     title: 'Ghostbusters',
     year: 1984,
     director: 'Ivan Reitman',
+    watched: true,
+  },
+  {
+    id: 2,
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: '',
+    watched: false,
+  },
+  {
+    id: 3,
+    title: 'The Dark Knight',
+    year: 2008,
+    director: 'Christopher Nolan',
     watched: false,
   },
 ];
